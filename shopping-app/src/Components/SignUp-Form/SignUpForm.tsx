c6b393c4c66d18a1a8394ad0c66d62aab40c8f08
Abeku <acobbina717@gmail.com>
2022-11-27 22:58:09 -0500

    removed test div

--- shopping-app/src/Components/SignUp-Form/SignUpForm.tsx
+++ shopping-app/src/Components/SignUp-Form/SignUpForm.tsx
@@ -67,7 +67,6 @@ const SignUpForm = () => {
 
   return (
     <SignUpContainer>
-      <div></div>
       <h2>Don't have an account?</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
