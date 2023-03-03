057c266c1f216e790515d9a2e71bdf9af6780c96
Abeku <acobbina717@gmail.com>
2023-03-03 03:21:02 -0500

    sign-in api route

---
+++ pages/api/signin.ts
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../src/utils/prisma";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email,
+        time: Date.now(),
+      },
+      "secret",
+      {
+        expiresIn: "6h",
+      }
+    );
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("SHOPPING-COOKIE", token, {
+        httpOnly: true,
+        maxAge: 6 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+    res.json(user);
+  } else {
+    res.status(401);
+    res.json({ error: "Wrong email or password" });
+  }
+};
