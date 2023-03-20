a474ebfcce5e437c24170636bc78ac5236399b92
Abeku <acobbina717@gmail.com>
2023-03-20 14:32:38 -0400

    nextAuth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,31 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
 // eslint-disable-next-line no-restricted-exports
 // export { default } from "next-auth/middleware";
 // import { NextRequest, NextResponse } from "next/server";
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "./pages/api/auth/[...nextauth]";
-
+// eslint-disable-next-line no-restricted-exports
+export { default } from "next-auth/middleware";
 export const config = { matcher: ["/checkout"] };
 
-const authenticatedPages = ["/checkout"];
+// const authenticatedPages = ["/checkout"];
 
-export default function middleware(req: NextRequest) {
-  if (authenticatedPages.find((p) => p === req.nextUrl.pathname)) {
-    const env = process.env.NODE_ENV;
-    let token;
+// export default function middleware(req: NextRequest) {
+//   if (authenticatedPages.find((p) => p === req.nextUrl.pathname)) {
+//     const env = process.env.NODE_ENV;
+//     let token;
 
-    if (env === "production") {
-      token = req.cookies.get("__Secure-next-auth.session-token");
-    }
-    token = req.cookies.get("next-auth.session-token");
+//     if (env === "production") {
+//       token = req.cookies.get("__Secure-next-auth.session-token");
+//     }
+//     token = req.cookies.get("next-auth.session-token");
 
-    const url = req.nextUrl.clone();
-    url.pathname = "/auth";
+//     const url = req.nextUrl.clone();
+//     url.pathname = "/auth";
 
-    if (!token) {
-      return NextResponse.redirect(url);
-    }
-  }
-}
+//     if (!token) {
+//       return NextResponse.redirect(url);
+//     }
+//   }
+// }
